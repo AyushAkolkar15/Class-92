a4541bd8a3019760c40c94b6e2fa870e17f359bb
AyushAkolkar15 <121815916+AyushAkolkar15@users.noreply.github.com>
2023-12-02 20:00:10 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC4nBFXk_bi5LJ_DgT5FPUKPCTBoKMUqqM",
+  authDomain: "bookworm-de24b.firebaseapp.com",
+  databaseURL: "https://bookworm-de24b-default-rtdb.firebaseio.com",
+  projectId: "bookworm-de24b",
+  storageBucket: "bookworm-de24b.appspot.com",
+  messagingSenderId: "118473256549",
+  appId: "1:118473256549:web:a3abb75405a19ae678e08c"
+};
